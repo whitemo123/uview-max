8ca2d9afacc2b7f7a5e18f5234d6b1eaaffa4b1a
classmateliu <2813089832@qq.com>
2025-02-18 09:20:33 +0800

    修改dayjs的导入方式

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,47 +1,12 @@
 <template>
-  <view>
-    <!--    <u-calendar-old v-model="show" mode="range"></u-calendar-old>-->
-    <u-radio-group
-      v-model="radioValue"
-      placement="column"
-      @change="groupChange"
-    >
-      <u-radio
-        @change="radioChange"
-        name="123"
-        :isCancel="true"
-      ></u-radio>
-      <u-radio
-        @change="radioChange"
-        name="1234"
-      ></u-radio>
-    </u-radio-group>
-    {{radioValue}}
-  </view>
+	<view>
+		<u-calendar :show="show"></u-calendar>
+		<u-button @click="show = true">打开</u-button>
+	</view>
 </template>
-<script setup lang="ts">
-import {ref, reactive, computed} from 'vue';
-// import UCalendarOld from "@/uview-max/components/u-calendar-old/u-calendar-old.vue"
 
-const show = ref(true)
-const radioValue = ref('')
-const groupChange = (e:any) => {
-  console.log(e)
-}
-const radioChange = (e:any) => {
-  console.log(e)
-}
+<script lang="ts" setup>
+import { ref } from 'vue';
+
+const show = ref(false);
 </script>
-<style lang="scss" scoped>
-.goods {
-  width: 100%;
-
-  image {
-    width: 100%;
-  }
-
-  view {
-    width: 100%;
-  }
-}
-</style>
